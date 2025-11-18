8efbcdc78f43ce7c84897e35b7b96aaf37fe46e6
berdnaski <berdnaski1177@gmail.com>
2025-11-18 11:04:09 -0300

    chore: config initial project

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./app/**/*.{js,jsx,ts,tsx}",
+  ],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      fontFamily: {
+        'display': ['Clash Display', 'System'],
+      },
+    },
+  },
+  plugins: [],
+}
