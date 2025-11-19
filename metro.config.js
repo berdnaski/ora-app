f2fe7ef235411302a114467c66c6b52db65630f5
berdnaski <berdnaski1177@gmail.com>
2025-11-18 21:18:00 -0300

    fix: change config project

--- metro.config.js
+++ metro.config.js
@@ -2,6 +2,10 @@ const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
+config.transformer = {
+  ...config.transformer,
+  unstable_allowRequireContext: true,
+};
 
 module.exports = withNativeWind(config, { input: './src/styles/global.css' });
 
